4817975aa54ddbd70d853a0e268f20c59c91e7a7
NachoR9 <nachorm9@hotmail.com>
2024-07-13 19:16:02 -0700

    fixing the text of the tests

--- test/array.test.js
+++ test/array.test.js
@@ -5,13 +5,13 @@ describe('arrSum', () => {
     it('should return 21 when adding the two arrays', () => {
         expect(arrSum([1, 2, 3],[4, 5, 6])).toBe(21);
     });
-    it('should return 21 when adding the two arrays', () => {
+    it('should return -21 when adding the two arrays', () => {
         expect(arrSum([-1, -2, -3],[-4, -5, -6])).toBe(-21);
     });
-    it('should return 21 when adding the two arrays', () => {
+    it('should return 15 when adding the two arrays', () => {
         expect(arrSum([0, 0, 0],[4, 5, 6])).toBe(15);
     });
-    it('should return 21 when adding the two arrays', () => {
+    it('should return 2100 when adding the two arrays', () => {
         expect(arrSum([100, 200, 300],[400, 500, 600])).toBe(2100);
     });
 })
